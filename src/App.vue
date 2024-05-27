47d1fb388016a8b7eeef32460bb64fd1c87c4dc3
Antonino <drakon9927@libero.it>
2024-05-27 16:19:02 +0200

    Template

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+</script>
+
+<template>
+  <div>
+    <app-header></app-header>
+    <app-main></app-main>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <img :src="logo" alt="Logo">
+        <h4>Yu-Gi-OH Api</h4>
+    </header>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            logo: 'public/img/yugi.png'
+        };
+    }
+}
+</script>
+
+<style scoped>
+header {
+    height: 3rem;
+}
+</style>
