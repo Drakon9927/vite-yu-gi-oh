a8bb469a4eba6270614dbc4dc1c6390886f32fe9
Antonino <drakon9927@libero.it>
2024-05-28 16:04:06 +0200

    m0

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,39 +1,39 @@
-<script>
-export default {
-    data() {
-        return {
-            
-        };
-    }
-}
-</script>
 
 
 <template>
     <main>
-        <div class="container-fluid w-100 mybgc">
-            <div class="container mymaxwidth p-4">
-                <select id="exampleSelect">
-                <option value="opzione1">Alien</option>
-                <option value="opzione2">Drago</option>
-                </select>
-            <div class="container mymaxwidth">
-
-            </div>
-            </div>
-
+      <div class="container-fluid w-100 mybgc">
+        <div class="container mymaxwidth p-4">
+          <select id="exampleSelect">
+            <option value="opzione1">Alien</option>
+            <option value="opzione2">Drago</option>
+          </select>
+          <div class="container mymaxwidth">
+          
+            <cards-list></cards-list>
+          </div>
         </div>
-        
+      </div>
     </main>
-</template>
-
-<style scoped>
-.mybgc{
+  </template>
+  
+  <script>
+  import CardsList from './CardsList.vue'; 
+  
+  export default {
+    components: {
+      CardsList 
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .mybgc {
     background-color: #C08234;
     height: 100vh;
-}
-
-.mymaxwidth{
+  }
+  
+  .mymaxwidth {
     max-width: 75%;
-}
-</style>
+  }
+  </style>
--- src/components/CardsList.vue
+++ src/components/CardsList.vue
@@ -1,25 +1,28 @@
 <template>
-    <div>
-      <single-card
-        v-for="card in dati"
-        :key="card.name"
-        :card="card"
-      ></single-card>
-    </div>
-  </template>
-  
-  <script>
-  import { dati } from './carte.js'; 
-  
-  export default {
-    name: 'CardsList',
-    components: {
-      SingleCard: () => import('./SingleCard.vue') // 
-    },
-    data() {
-      return {
-        dati
-      };
-    }
+  <div>
+    <single-card
+      v-for="card in carte"
+      :key="card.id"  
+      :card="card"
+    ></single-card>
+  </div>
+</template>
+
+<script>
+import { store } from '../store.js';
+
+export default {
+  name: 'CardsList',
+  components: {
+    SingleCard: () => import('./SingleCard.vue')
+  },
+  created() {
+    store.fetchCarte(); 
+  },
+  data() {
+    return {
+      carte: store.carte
+    };
   }
-  </script>
+}
+</script>
